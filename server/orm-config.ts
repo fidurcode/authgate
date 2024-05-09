236636ee5903d30d7d2ac9767a5ed9a65890772e
fidurcode <dev@fidurcode.pl>
2024-05-09 06:27:13 +0200

    Correct docker-compose and fix some bugs.

---
+++ server/orm-config.ts
@@ -0,0 +1,15 @@
+import { User } from 'src/entities/user.entity';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+const config: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: 'db',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'postgres',
+  entities: [User],
+  synchronize: true,
+};
+
+export default config;
